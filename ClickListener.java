f2c1f846ce41e8c720011dcc6ae81dd1af7db542
Matthew Callaghan <mcallaghan@Naperville203.org>
2021-04-08 08:37:59 -0500

    finished GUI notes for 6th

---
+++ ClickListener.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ * Write a description of class ClickListener here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ClickListener implements ActionListener
+{
+    @Override
+    public void actionPerformed(ActionEvent event)
+    {
+        System.out.println("button clicked");
+    
+    }
+}
